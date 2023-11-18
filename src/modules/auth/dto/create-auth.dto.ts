8e306ed0f269b2acb1c0e2190bb455ac47a49086
unknown <lelenahkht2003@gamil.com>
2023-11-18 15:53:49 +0700

    Implement data validation

--- src/modules/auth/dto/create-auth.dto.ts
+++ src/modules/auth/dto/create-auth.dto.ts
@@ -1,10 +1,35 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 export class CreateAuthDto {
+  @IsNotEmpty()
+  @IsString()
   username: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
+
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   phone: string;
+
+  @IsNotEmpty()
+  @IsString()
   gender: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   age: number;
+
+  @IsNotEmpty()
+  @IsString()
   address: string;
 }
--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,10 +1,35 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsPhoneNumber, IsString } from "class-validator";
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   username: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
+
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   phone: string;
+
+  @IsNotEmpty()
+  @IsString()
   gender: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   age: number;
+
+  @IsNotEmpty()
+  @IsString()
   address: string;
 }
